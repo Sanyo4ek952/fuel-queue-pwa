a137e7f6be5aa6df03b682f10572cc3265fe28c0
Sanyo4ek958 <sanyo4ek952@gmail.com>
2026-07-09 21:37:08 +0300

    commit

---
+++ supabase/migrations/20260709210000_update_station_names.sql
@@ -0,0 +1,20 @@
+update public.stations
+set
+  name = 'АТАН АЗС №076 (нижняя)',
+  address = 'Восточное шоссе, 2',
+  is_active = true
+where id = '10000000-0000-0000-0000-000000000001';
+
+update public.stations
+set
+  name = 'АТАН АЗС №077 (верхняя)',
+  address = 'Феодосийское шоссе, 14',
+  is_active = true
+where id = '10000000-0000-0000-0000-000000000002';
+
+update public.stations
+set
+  name = 'ТЭС АЗС №37',
+  address = 'Феодосийское шоссе, 12А',
+  is_active = true
+where id = '10000000-0000-0000-0000-000000000003';
